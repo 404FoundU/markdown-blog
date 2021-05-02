8346ed47d4e5a8dc3b9b487ac7f9613613aff09c
unni <unnikrishnanrp@outlook.com>
2021-05-02 10:21:13 -0500

    created form

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 const router = express.Router();
-router.route("/").get((req, res) => {
-    // res.send("Hello world");
-    res.send("articles");
-});
+
 router.get('/new', (req, res) =>{
     res.render('articles/new');
+})
+
+router.post('/', (req,res) =>{
+
 })
 export default router;
