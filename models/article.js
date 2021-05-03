db9ab018858d762b48e575f69b88656c3efe6e07
upillai <upillai@xpressdocs.com>
2021-05-03 11:22:18 -0500

    Added slugify to modify urls to (test-test)

--- models/article.js
+++ models/article.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import marked from "marked";
+import slugify from "slugify";
 
 const articleSchema = new mongoose.Schema({
         title: {
@@ -15,8 +17,23 @@ const articleSchema = new mongoose.Schema({
         createdAt: {
             type: Date,
             default: Date.now
-        }
+        },
+    slug: {
+            type:String,
+        required: true,
+        unique:true
+    }
+
     }
 )
+
+articleSchema.pre('validate', () =>{
+    if (this.title) {
+        this.slug= slugify(this.title, {
+            strict: true,
+            lower: true
+        })
+    }
+})
 const Blog=  mongoose.model('Blog', articleSchema);
 export default Blog;
