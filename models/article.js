26fd222f44ac1c73d363a69c450cd5b3454abcc7
upillai <upillai@xpressdocs.com>
2021-05-03 13:29:21 -0500

    Added sanitizedHtml

--- models/article.js
+++ models/article.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import marked from "marked";
 import slugify from "slugify";
+import createDomPurify from 'dompurify';
+import {JSDOM} from 'jsdom';
 
+const dompurify = createDomPurify(new JSDOM().window)
 const articleSchema = new mongoose.Schema({
         title: {
             type: String,
@@ -22,15 +25,22 @@ const articleSchema = new mongoose.Schema({
             type: String,
             required: true,
             unique: true
+        },
+        sanitizedHtml: {
+            type: String,
+            required: true
         }
     }
-)
+);
 articleSchema.pre('validate', function (next) {
     if (this.title) {
         this.slug = slugify(this.title, {
             strict: true,
             lower: true
         })
+    }
+    if (this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
     }
     next();
 })
